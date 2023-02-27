b57503665e25ec3bc3534c56f14655ee2dcc3a4a
delijohn <nikolaos.deligiannis@polito.it>
2023-02-27 23:16:19 +0100

    Implemented Net Grammar & Parser

--- adjacent_nodes_finder.py
+++ adjacent_nodes_finder.py
@@ -10,12 +10,14 @@ LastEditTime: 2023-02-25 23:06:43
 @ License: MIT
 '''
 
-from lark import Lark
-from lark import Transformer
+from lark import Lark, Transformer, v_args
+from dataclasses import dataclass
+from copy import copy, deepcopy
 import math
-from typing import List, Dict, Tuple, Any
+from typing import List, Dict, Tuple, Any, Union
 import random
 import sys
+import re 
 import argparse as ap
 
 functional_unit_list={"adder":"u_ibex_core/ex_block_i/alu_i/alu_32bit_adder/",
@@ -24,32 +26,32 @@ functional_unit_list={"adder":"u_ibex_core/ex_block_i/alu_i/alu_32bit_adder/",
                  "decoder":"u_ibex_core/id_stage_i/decoder_i/"}
 
 node_list=[] #contains all the node in the SoC
-
+#
 net_grammar=r"""
-    net_list: net*
 
-    net: "-" net_name ["(" "PIN" pin_name ")"] net_aliasing_list ["+" regular_wiring_statement] ";"
-    net_name: /(\w|\/|\[|\])+/
-    pin_name: /(\w)+\[[0-9]+\]/
+    net_list: net+
 
-    //net_name_alias
-    net_aliasing_list: net_name_alias*
-    net_name_alias: "(" component_path net_alias ")"
-    component_path: /(\w|\/)+/
-    net_alias: CNAME
+    net: "-" NET_NAME port_list element_list ";" 
+    ?port_list: pin_or_port+
+    ?element_list: regular_wiring_statement*
+    NET_NAME: /[^\s]+/ 
 
-    regular_wiring_statement: (wire)+ 
-    wire: WIRE_TYPE layer "(" x y EXT_VAL? ")" ["(" x_ y_ EXT_VAL_? ")"] [via_name]  
-    WIRE_TYPE: ("ROUTED"|"NEW")~1
-    layer: CNAME
-    x: NUMBER
-    y: NUMBER
-    EXT_VAL: NUMBER
-    x_: NUMBER|/\*/
-    y_: NUMBER|/\*/
-    EXT_VAL_: NUMBER
-    via_name: CNAME
+    ?pin_or_port: "(" (pin | port) ")"
+    pin: "PIN" PIN_NAME
+    port: COMPONENT_NAME PIN_NAME 
 
+    PIN_NAME: /[^\s]+/
+    COMPONENT_NAME: /(?!PIN)[^\s]+/
+
+    regular_wiring_statement: "+" routing_element+
+    routing_element: WIRING_KEYWORD LAYER_NAME routing_point routing_point? VIA_NAME?
+    routing_point: "(" COORDINATE COORDINATE EXTENSION? ")" 
+    WIRING_KEYWORD: "COVER" | "FIXED" | "ROUTED" | "NOSHIELD" | "NEW"  
+    VIA_NAME: /(?!NEW)[^\s]+/
+    EXTENSION: /\d+/
+    COORDINATE: /\d+/ | "*"
+    LAYER_NAME: /\w+\d+/
+   
     %import common.SIGNED_NUMBER    
     %import common.NUMBER           
     %import common.CNAME            
@@ -57,110 +59,211 @@ net_grammar=r"""
     %ignore WS
     """
 
-class soc_node:
-    name=None
-    layout=None
-    location=[0,0]
+@dataclass 
+class RoutingPoint:
+    first_coordinate: Union[int, str]
+    second_coordinate: Union[int, str]  
 
-    def __init__(self, name, layout, location) -> None:
-        self.name=name
-        self.layout=layout
-        self.location=location
-    
-    def check_name(self):
-        print(self.name)
+    def __repr__(self) -> str:
+        return f"(x,y) = {self.first_coordinate} {self.second_coordinate}"
 
-    def check_layout(self):
-        print(self.layout)
+@dataclass 
+class RoutingElement: 
+    metal_layer: str
+    via: str 
+    starting_point: RoutingPoint
+    ending_point: RoutingPoint
 
-    def check_location(self):
-        print(f"###############################")
-        print(f"x={self.location[0]}")
-        print(f"y={self.location[1]}")
+    def normalize(self) -> "RoutingElement":
+        if self.ending_point is None: 
+            return self 
+    
+        starting_point = copy(self.starting_point)
+        ending_point = copy(self.ending_point)
+   
+        if starting_point.first_coordinate == '*':
+            starting_point.first_coordinate = ending_point.first_coordinate
+        if starting_point.second_coordinate == '*':
+            starting_point.second_coordinate = ending_point.second_coordinate
 
-# {net_name: [pin_name, aliasing_dict{component: alias}, wire_list[[wire_type, layer, start_point], ...]], ...}
-class data_transformer(Transformer):
-    def net_list(self, nets):
-        return dict(nets)
-    
-    def net(self, net_info):
-        net_name, other_info=net_info[0], net_info[1:]
-        return net_name, other_info
-    
-    def net_name(self, name):
-        return str(name[0])
-    
-    def pin_name(self, name):
-        return str(name[0])
-    
-    def net_aliasing_list(self, alias_list):
-        return dict(alias_list)
-    
-    def net_name_alias(self, alias_pair):
-        path, name=alias_pair[0], alias_pair[1] 
-        return path, name
-        
-    def component_path(self, path):
-        return str(path[0])
+        if ending_point.first_coordinate == '*':
+            ending_point.first_coordinate = starting_point.first_coordinate
+        if ending_point.second_coordinate == '*':
+            ending_point.second_coordinate = starting_point.second_coordinate
 
-    def net_alias(self, name):
-        return str(name[0])
-    
-    def regular_wiring_statement(self, wire_list):
-        return list(wire_list)
-    
-    def wire(self, wire_info):
-        tp, layer, coordinate=wire_info[0], wire_info[1], [wire_info[2], wire_info[3]]
-        return tp, layer, coordinate
-    
-    def WIRE_TYPE(self, tp):
-        return str(tp)
-    
-    def layer(self, l):
-        return str(l[0])
-    
-    def x(self, n):
-        return str(n[0])
-    
-    def y(self, n):
-        return str(n[0])
+        return RoutingElement(self.metal_layer, self.via, starting_point, ending_point)
 
-def sorting(node:soc_node, functional_node_list:list)->Tuple[soc_node, float]:
-    # finding all the node in the same layout.
-    node_in_same_layout_list=[]
+    def __repr__(self) -> str:
+        if self.ending_point is None: 
+            return f"{self.metal_layer}: {self.starting_point}"
 
-    for n in functional_node_list:
-        if n.layout==node.layout:
-            node_in_same_layout_list.append(n) #[another node, distance to a specified node]
-    node_in_same_layout_list.remove(node) # remove node itself
+        normalized = self.normalize()
+        return f"{self.metal_layer}: {normalized.starting_point} -> {normalized.ending_point}"
 
-    # calculate euclidean distances between node (wire starting point) and other nodes (starting point)
-    nearest_node=node_in_same_layout_list[0]
-    distance=math.dist([float(x) for x in nearest_node.location], [float(x) for x in node.location])
+@dataclass
+class Port:
+    hierarchy_name: str
+    port: str 
 
-    for ele in node_in_same_layout_list[1:]:
-        cur_dis=math.dist([float(x) for x in ele.location], [float(x) for x in node.location])
-        if cur_dis<=distance:
-            nearest_node=ele
-            distance=cur_dis
+    def __repr__(self) -> str:
+        if self.hierarchy_name: 
+            return f"{self.hierarchy_name}/{self.port}"
+        else: 
+            return f"{self.port}"
 
-    return nearest_node, distance
+@dataclass
+class Net: 
+    net_name: str 
+    routing_ports: List[Port]
+    routing_elements: List[RoutingElement]
 
-def file_parser(file_name:str, targeted_functional_unit:str=None) -> List[soc_node]:
+    def __repr__(self) -> str:
+
+        def _compact_routing_element(element: RoutingElement) -> str:
+            normalized = element.normalize()
+            start = normalized.starting_point
+
+            if normalized.ending_point:
+                end = normalized.ending_point
+                return f"({start.first_coordinate},{start.second_coordinate},{end.first_coordinate},{end.second_coordinate})"
+            else: 
+                return f"({start.first_coordinate},{start.second_coordinate})"
+            
+        return f"{self.net_name}: {' -> '.join([_compact_routing_element(x) for x in self.routing_elements])}"
+  
+
+class DefNetsTransformer(Transformer):
+    
+    @v_args(inline=True)
+    def routing_point(self, first_coordinate: str, second_coordinate: str, extension: str = None) -> Any: 
+        """
+        >>> data = "( 37050 359660 2 )"
+        >>> Lark(net_grammar, parser='lalr', start='routing_point', transformer=DefNetsTransformer()).parse(data)
+        (x,y) = 37050 359660
+        >>> data = "( 37050 * 2 )"
+        >>> Lark(net_grammar, parser='lalr', start='routing_point', transformer=DefNetsTransformer()).parse(data)
+        (x,y) = 37050 *
+        >>> data = "( 37050 23245 )"
+        >>> Lark(net_grammar, parser='lalr', start='routing_point', transformer=DefNetsTransformer()).parse(data)
+        (x,y) = 37050 23245
+        """
+        return RoutingPoint(int(first_coordinate) if first_coordinate != '*' else '*',
+                            int(second_coordinate) if second_coordinate != '*' else '*')
+
+    @v_args(inline=True)
+    def routing_element(self, keyword:str, metal_layer: str, starting_point:RoutingPoint, ending_point: RoutingPoint = None, via: str = None ) -> RoutingElement:
+        """
+        >>> data = "ROUTED metal2 ( 37050 359660 ) ( * 361340 ) via1_4"
+        >>> Lark(net_grammar, parser='lalr', start='routing_element', transformer=DefNetsTransformer()).parse(data)
+        metal2: (x,y) = 37050 359660 -> (x,y) = 37050 361340
+        """
+        # FIXME: The routing_element rule must be fixed
+        # Point has one coordinate only
+        if isinstance(ending_point, str):    
+            return RoutingElement(metal_layer, ending_point, starting_point, None)
+        else:
+            return RoutingElement(metal_layer, via, starting_point, ending_point)
+    
+    def regular_wiring_statement(self, list_of_routes: List[RoutingElement]) -> List[RoutingElement]: 
+        """
+        >>> data = "+ ROUTED metal2 ( 37050 359660 ) ( * 361340 ) via1_4 NEW metal4 ( 261890 56420 ) ( 262450 * );"
+        >>> Lark(net_grammar, parser='lalr', start='regular_wiring_statement', transformer=DefNetsTransformer()).parse(data)
+        [metal2: (x,y) = 37050 359660 -> (x,y) = 37050 361340, metal4: (x,y) = 261890 56420 -> (x,y) = 262450 56420]
+        """
+        return list_of_routes
+
+    def pin_or_port(self, port: Port) -> Port: 
+        """
+        >>> data = "( PIN ram_cfg_i[9] )"
+        >>> Lark(net_grammar, parser='lalr', start='pin_or_port', transformer=DefNetsTransformer()).parse(data)
+        ram_cfg_i[9]
+        >>> data = "( u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1882 CO )"
+        >>> Lark(net_grammar, parser='lalr', start='pin_or_port', transformer=DefNetsTransformer()).parse(data)
+        u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1882/CO
+        """      
+        return port 
+    
+    @v_args(inline=True)
+    def pin(sef, pin_name: str) -> Port: 
+        """
+        >>> data = "( PIN ram_cfg_i[9] )"
+        >>> Lark(net_grammar, parser='lalr', start='pin_or_port', transformer=DefNetsTransformer()).parse(data)
+        ram_cfg_i[9]
+        """      
+        return Port(None, pin_name)
+    
+    @v_args(inline=True)
+    def port(self, hierarchy_name: str, port_name: str) -> Port: 
+        """
+        >>> data = "( u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1882 CO )"
+        >>> Lark(net_grammar, parser='lalr', start='pin_or_port', transformer=DefNetsTransformer()).parse(data)
+        u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1882/CO
+        """      
+        return Port(hierarchy_name, port_name)
+    
+    @v_args(inline=True)
+    def net(self, net_name: str, ports: List[Port], elements: List[RoutingElement]) -> Net: 
+        """
+        >>> data = '''- u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/n1764
+        ... ( u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1882 CO )
+        ... ( u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1874 A )
+        ... + ROUTED metal3 ( 308370 89180 ) ( 310650 * ) via2_5
+        ...   NEW metal4 ( 307810 124740 ) ( 308370 * )
+        ...   NEW metal4 ( 308090 90580 ) ( 308370 * )
+        ...   NEW metal4 ( 308370 89180 ) via3_2
+        ...   NEW metal3 ( 308370 144060 ) via2_5
+        ...   NEW metal2 ( 310650 86940 ) via1_4
+        ... ;'''
+        >>> Lark(net_grammar, parser='lalr', start='net', transformer=DefNetsTransformer()).parse(data)
+        u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/n1764: (308370,89180,310650,89180) -> (307810,124740,308370,124740) -> (308090,90580,308370,90580) -> (308370,89180) -> (308370,144060) -> (310650,86940)
+        """      
+        return Net(net_name, ports, elements)
+
+    def port_list(self, ports: List[Port]) -> List[Port]:
+        """
+        >>> data = '''( u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1882 CO )
+        ... ( u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1874 A )'''
+        >>> Lark(net_grammar, parser='lalr', start='port_list', transformer=DefNetsTransformer()).parse(data)
+        [u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1882/CO, u_ibex_core/ex_block_i/gen_multdiv_fast_multdiv_i/U1874/A]
+        """    
+        return ports 
+    
+    def element_list(self, elements: List[RoutingElement]) -> List[RoutingElement]:
+        """
+        >>> data = '''+ ROUTED metal3 ( 308370 89180 ) ( 310650 * ) via2_5
+        ...   NEW metal4 ( 307810 124740 ) ( 308370 * )
+        ...   NEW metal4 ( 308090 90580 ) ( 308370 * )
+        ...   NEW metal4 ( 308370 89180 ) via3_2
+        ...   NEW metal3 ( 308370 144060 ) via2_5
+        ...   NEW metal2 ( 310650 86940 ) via1_4'''
+        >>> Lark(net_grammar, parser='lalr', start='element_list', transformer=DefNetsTransformer()).parse(data) # doctest +NORMALIZE_WHITESPACE
+        [metal3: (x,y) = 308370 89180 -> (x,y) = 310650 89180, metal4: (x,y) = 307810 124740 -> (x,y) = 308370 124740, metal4: (x,y) = 308090 90580 -> (x,y) = 308370 90580, metal4: (x,y) = 308370 89180, metal3: (x,y) = 308370 144060, metal2: (x,y) = 310650 86940]
+        """        
+        return elements
+
+def file_parser(file_name:str, targeted_functional_unit:str=None) -> Any:
     all_node={}
 
     # read the file which is our target
     with open(file_name, "r") as input_file:
         data = input_file.read()
-    net_input = data[data.find("NETS") + 13 : data.find("END NETS")]
+    #net_input = data[data.find("NETS") + 13 : data.find("END NETS")]
     
-    # parse the doc.def and get all the useful information of each node
-    net_parser = Lark(net_grammar, start='net_list')
-    net_data = net_parser.parse(net_input)
+    match = re.search(r'\bNETS\b\s+\d+\s*;(.*)\bEND NETS\b', data, re.DOTALL | re.MULTILINE)
+    
+    if not match:
+        raise ValueError("Invalid input file")
 
+    print(data[:match.span(1)[0]].count('\n'))    
+    data = match.group(1)
+
+    net_parser = Lark(net_grammar, parser='lalr', start='net_list', transformer=DefNetsTransformer())
+    net_data = net_parser.parse(data)
+    print(net_data)
+    exit(0)
     # transform data to a dictionary
     all_node=data_transformer().transform(net_data)
-    # print(all_node)
+    print(all_node)
     # print(targeted_functional_unit)
     
     # extract nodes that are needed
